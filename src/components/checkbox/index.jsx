9e1bcc7732822ca2475c172fbcc4c0c7056685f1
Johannes Ström <johannes.strom@svenskaspel.se>
2022-10-21 22:32:17 +0200

    added semi-colon

--- src/components/checkbox/index.jsx
+++ src/components/checkbox/index.jsx
@@ -4,7 +4,7 @@ import { StyledChekbox, initCheckbox, toggleChek } from './imports';
 import { useEffect, useRef } from 'react';
 
 export function Checkbox({name, checked=false, callback = ()=>{}}) {
-  const checkBoxAtom = useRef(atom({name, checked, callback}))
+  const checkBoxAtom = useRef(atom({name, checked, callback}));
   const [checkList, setCheckList] = useAtom(chekboxList);
   const [checkBox, setCheckBox] = useAtom(checkBoxAtom.current);
   
