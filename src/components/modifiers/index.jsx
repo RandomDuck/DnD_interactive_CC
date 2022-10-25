da7da88886635588d0b2cfd80a3ee5255fcca1a1
Johannes Ström <johannes.strom@svenskaspel.se>
2022-10-25 10:43:21 +0200

    modified input mod to show score on load

--- src/components/modifiers/index.jsx
+++ src/components/modifiers/index.jsx
@@ -4,14 +4,14 @@ import { useAtom } from 'jotai';
 import { StyledInput, StyledSpan, calcAbilityWithProf } from './imports';
 
 export function ModInput({type, size, underline=false}) {
-  const [,setMod] = useAtom(getModifierAtom(type))
-  return <StyledInput type="number" underline={underline} onChange={el=>setMod(el.target.valueAsNumber)} size={size} />;
+  const [mod ,setMod] = useAtom(getModifierAtom(type))
+  return <StyledInput type="number" underline={underline} value={mod} onChange={el=>setMod(el.target.valueAsNumber)} size={size} />;
 }
 
-export function ModDisplay({type, classes, boxName, text="", displayScore=false}) {
+export function ModDisplay({type, classes, boxName, showAtr, text="", displayScore=false}) {
   const [getList] = useAtom(chekboxList);
   const [getMod] = useAtom(getModifierAtom(type));
   const [getProfMod] = useAtom(getModifierAtom('prof'));
   
-  return <StyledSpan className={classes} >{text}{displayScore ? getMod : calcAbilityWithProf(getMod, getList, boxName, getProfMod)}</StyledSpan>;
+  return <StyledSpan className={classes} >{text}{displayScore ? getMod : calcAbilityWithProf(getMod, getList, boxName, getProfMod)}{showAtr && ` (${type})`}</StyledSpan>;
 }
